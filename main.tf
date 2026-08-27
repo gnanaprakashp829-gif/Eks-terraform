08a9620de8bbe6937933626a8490bda11a78750e
Gnana prakash <gnanaprakashp829@gmail.com>
2026-08-27 23:59:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,55 +2,76 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_vpc" "devopsshack_vpc" {
+# -------------------------
+# VPC
+# -------------------------
+resource "aws_vpc" "prakash_vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "devopsshack-vpc"
+    Name = "prakash-vpc"
   }
 }
 
-resource "aws_subnet" "devopsshack_subnet" {
+# -------------------------
+# Subnets
+# -------------------------
+resource "aws_subnet" "prakash_subnet" {
   count = 2
-  vpc_id                  = aws_vpc.devopsshack_vpc.id
-  cidr_block              = cidrsubnet(aws_vpc.devopsshack_vpc.cidr_block, 8, count.index)
+
+  vpc_id                  = aws_vpc.prakash_vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.prakash_vpc.cidr_block, 8, count.index)
   availability_zone       = element(["ap-south-1a", "ap-south-1b"], count.index)
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "devopsshack-subnet-${count.index}"
+    Name = "prakash-subnet-${count.index}"
   }
 }
 
-resource "aws_internet_gateway" "devopsshack_igw" {
-  vpc_id = aws_vpc.devopsshack_vpc.id
+# -------------------------
+# Internet Gateway
+# -------------------------
+resource "aws_internet_gateway" "prakash_igw" {
+  vpc_id = aws_vpc.prakash_vpc.id
 
   tags = {
-    Name = "devopsshack-igw"
+    Name = "prakash-igw"
   }
 }
 
-resource "aws_route_table" "devopsshack_route_table" {
-  vpc_id = aws_vpc.devopsshack_vpc.id
+# -------------------------
+# Route Table
+# -------------------------
+resource "aws_route_table" "prakash_route_table" {
+  vpc_id = aws_vpc.prakash_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.devopsshack_igw.id
+    gateway_id = aws_internet_gateway.prakash_igw.id
   }
 
   tags = {
-    Name = "devopsshack-route-table"
+    Name = "prakash-route-table"
   }
 }
 
+# -------------------------
+# Route Table Association
+# -------------------------
 resource "aws_route_table_association" "a" {
-  count          = 2
-  subnet_id      = aws_subnet.devopsshack_subnet[count.index].id
-  route_table_id = aws_route_table.devopsshack_route_table.id
+  count = 2
+
+  subnet_id      = aws_subnet.prakash_subnet[count.index].id
+  route_table_id = aws_route_table.prakash_route_table.id
 }
 
-resource "aws_security_group" "devopsshack_cluster_sg" {
-  vpc_id = aws_vpc.devopsshack_vpc.id
+# -------------------------
+# EKS Cluster Security Group
+# -------------------------
+resource "aws_security_group" "prakash_cluster_sg" {
+  name   = "prakash-cluster-sg"
+  vpc_id = aws_vpc.prakash_vpc.id
 
   egress {
     from_port   = 0
@@ -60,13 +81,18 @@ resource "aws_security_group" "devopsshack_cluster_sg" {
   }
 
   tags = {
-    Name = "devopsshack-cluster-sg"
+    Name = "prakash-cluster-sg"
   }
 }
 
-resource "aws_security_group" "devopsshack_node_sg" {
-  vpc_id = aws_vpc.devopsshack_vpc.id
+# -------------------------
+# EKS Node Security Group
+# -------------------------
+resource "aws_security_group" "prakash_node_sg" {
+  name   = "prakash-node-sg"
+  vpc_id = aws_vpc.prakash_vpc.id
 
+  # Learning/lab configuration
   ingress {
     from_port   = 0
     to_port     = 0
@@ -82,94 +108,131 @@ resource "aws_security_group" "devopsshack_node_sg" {
   }
 
   tags = {
-    Name = "devopsshack-node-sg"
+    Name = "prakash-node-sg"
   }
 }
 
-resource "aws_eks_cluster" "devopsshack" {
-  name     = "devopsshack-cluster"
-  role_arn = aws_iam_role.devopsshack_cluster_role.arn
+# -------------------------
+# EKS Cluster IAM Role
+# -------------------------
+resource "aws_iam_role" "prakash_cluster_role" {
+  name = "prakash-cluster-role"
 
-  vpc_config {
-    subnet_ids         = aws_subnet.devopsshack_subnet[*].id
-    security_group_ids = [aws_security_group.devopsshack_cluster_sg.id]
-  }
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
 }
 
-resource "aws_eks_node_group" "devopsshack" {
-  cluster_name    = aws_eks_cluster.devopsshack.name
-  node_group_name = "devopsshack-node-group"
-  node_role_arn   = aws_iam_role.devopsshack_node_group_role.arn
-  subnet_ids      = aws_subnet.devopsshack_subnet[*].id
-
-  scaling_config {
-    desired_size = 3
-    max_size     = 3
-    min_size     = 3
-  }
-
-  instance_types = ["t2.medium"]
-
-  remote_access {
-    ec2_ssh_key = var.ssh_key_name
-    source_security_group_ids = [aws_security_group.devopsshack_node_sg.id]
-  }
-}
-
-resource "aws_iam_role" "devopsshack_cluster_role" {
-  name = "devopsshack-cluster-role"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "eks.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy_attachment" "devopsshack_cluster_role_policy" {
-  role       = aws_iam_role.devopsshack_cluster_role.name
+# EKS Cluster Policy
+resource "aws_iam_role_policy_attachment" "prakash_cluster_role_policy" {
+  role       = aws_iam_role.prakash_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-resource "aws_iam_role" "devopsshack_node_group_role" {
-  name = "devopsshack-node-group-role"
+# -------------------------
+# EKS Node IAM Role
+# -------------------------
+resource "aws_iam_role" "prakash_node_group_role" {
+  name = "prakash-node-group-role"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
 }
 
-resource "aws_iam_role_policy_attachment" "devopsshack_node_group_role_policy" {
-  role       = aws_iam_role.devopsshack_node_group_role.name
+# Worker Node Policy
+resource "aws_iam_role_policy_attachment" "prakash_node_group_role_policy" {
+  role       = aws_iam_role.prakash_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "devopsshack_node_group_cni_policy" {
-  role       = aws_iam_role.devopsshack_node_group_role.name
+# CNI Policy
+resource "aws_iam_role_policy_attachment" "prakash_node_group_cni_policy" {
+  role       = aws_iam_role.prakash_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
-resource "aws_iam_role_policy_attachment" "devopsshack_node_group_registry_policy" {
-  role       = aws_iam_role.devopsshack_node_group_role.name
+# ECR Read Only
+resource "aws_iam_role_policy_attachment" "prakash_node_group_registry_policy" {
+  role       = aws_iam_role.prakash_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+# -------------------------
+# EKS Cluster
+# -------------------------
+resource "aws_eks_cluster" "prakash" {
+  name     = "prakash-cluster"
+  role_arn = aws_iam_role.prakash_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = aws_subnet.prakash_subnet[*].id
+
+    security_group_ids = [
+      aws_security_group.prakash_cluster_sg.id
+    ]
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.prakash_cluster_role_policy
+  ]
+}
+
+# -------------------------
+# EKS Node Group
+# -------------------------
+resource "aws_eks_node_group" "prakash" {
+  cluster_name = aws_eks_cluster.prakash.name
+
+  node_group_name = "prakash-node-group"
+
+  node_role_arn = aws_iam_role.prakash_node_group_role.arn
+
+  subnet_ids = aws_subnet.prakash_subnet[*].id
+
+  scaling_config {
+    desired_size = 3
+    min_size     = 3
+    max_size     = 3
+  }
+
+  instance_types = ["t3.medium"]
+
+  remote_access {
+    ec2_ssh_key = var.ssh_key_name
+
+    source_security_group_ids = [
+      aws_security_group.prakash_node_sg.id
+    ]
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.prakash_node_group_role_policy,
+    aws_iam_role_policy_attachment.prakash_node_group_cni_policy,
+    aws_iam_role_policy_attachment.prakash_node_group_registry_policy
+  ]
 }
